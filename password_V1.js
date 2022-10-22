783b31445902447fc5b22057aafa7e4b5e8eb978
JasubiPL <gameportal7@hotmail.com>
2022-10-22 02:32:12 -0500

    Version 1.0

--- password_V1.js
+++ password_V1.js
@@ -1,30 +1,28 @@
 let password = [];
 let passwordFinal = "";
-let longitud = 90;
-let abc = ["0","1","2","3","4","5","6","7","8","9",
+const btnGenerar = document.getElementById("generar").addEventListener("click", start);
+const abc = ["0","1","2","3","4","5","6","7","8","9",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];//62 caracteres
 
-
-function aleatorio(){
-    for(x=1;x<=longitud;x++){
+function start(){
+    let getLenght = document.getElementById("inputLenght").value;
+    for(x=1;x<=getLenght;x++){
         password.push(Math.floor(Math.random()*62));
     }
     
-    //console.log(password);
     conversor();
 }
 
 function conversor(){
-    //console.log(password);
     for(i=0;i<=password.length -1;i++){
         
         password[i] = abc[password[i]];
-        //console.log(password);
+        
     }
     
     
-    //console.log(password);
+    
     union();
 }
 
@@ -38,8 +36,15 @@ function union(){
 }
 
 function respuesta(){
+    let getPassword = document.getElementById("inputPassword");
+    let getLenght = document.getElementById("inputLenght");
+    getPassword.value = passwordFinal;
+    getLenght.value = "";
     console.log(passwordFinal);
+    //getPassword.value(passwordFinal);
+    password = [];
+    passwordFinal="";
 }
 
 
-console.log(aleatorio());
+//start();
